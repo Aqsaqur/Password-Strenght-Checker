1354b164cb9a980297e9c8f8adf6289485decfa3
Aqsa Qur <aqsaqurayshi@gmail.com>
2025-03-05 08:02:59 +0500

    first commit

---
+++ PasswordMeter.py
@@ -0,0 +1,63 @@
+import re
+import streamlit as st
+
+st.set_page_config(page_title="Password Strenght Checker: ", page_icon="🔒",layout="centered")
+
+st.markdown("""
+<style>
+            .main {text-align: center;}
+            .stTextInput {width: 60% !important; margin: auto;}
+            .stButtom button {width: 50%; background-color #4CAF50; color: white; font-size: 18px;}
+            .stButton button:hover { background-color: #45a049}
+</style>
+""", unsafe_allow_html=True)
+
+st.title("Password strenght Generator")
+st.white("Enter your password below to check its security level.")
+
+def check_password_strenght(password):
+    score = 0
+    feedback = []
+
+    if len(password) >= 8:
+        score += 1 
+    else:
+        feedback.append("Password should be atleast be 8 character long!!")
+
+    if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
+        score += 1
+    else:
+        feedback.append("Password should include both uppercase  [A-Z] & lowercase [a-z]")
+
+    if re.search(r"\d", password):
+        score += 1
+
+    else:
+        feedback.append("Password should include atleast(0-9) ")
+
+    if re.search(r"[!@#$%^&]", password):
+        score += 1
+    else:
+        feedback.append(" Include at least one specail character (!@#$%^&)")
+
+    if score == 4:
+        st.success("STRONGER PASSWORD - Your password is secure.")
+    elif score == 3:
+        st.info("MODERATE PASSWORD - cnsider imporving security by adding more feature")
+    else:
+        st.error("WEEK PASSWORD - Follow the suggestion below to strenght it.")
+
+    if feedback:
+        with st.expander("Impove Your Password"):
+            for item in feedback:
+                st.write(item)
+password = st.text_input("Enter your password", type="password", help="Ensure yut password is strong")
+
+if st.button("Check Strenght"):
+    if password:
+        check_password_strenght(password)
+    else:
+        st.warning("Please enter a password first!")
+
+    
+
